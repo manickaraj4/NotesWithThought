5d06757bb4949db7f547daee105f36306f018df3
Manickaraj K M <40318995+manickaraj4@users.noreply.github.com>
2025-06-10 11:35:21 +0000

    Added LB and listeners

--- vpcConfig/main.tf
+++ vpcConfig/main.tf
@@ -15,9 +15,116 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "TerraformManaged"
+  }
+}
+
+resource "aws_default_subnet" "default_1a" {
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "TerraformManaged"
+  }
+}
+
+resource "aws_default_subnet" "default_1b" {
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "TerraformManaged"
+  }
+}
+
+resource "aws_default_subnet" "default_1c" {
+  availability_zone = "ap-south-1c"
+
+  tags = {
+    Name = "TerraformManaged"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TerraformManaged"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_kube_master_ports" {
+  security_group_id = aws_security_group.alb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8443
+  ip_protocol       = "tcp"
+  to_port           = 8443
+}
+
+resource "aws_lb" "master_lb" {
+  name               = "masterlb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [ aws_default_subnet.default_1a.id, aws_default_subnet.default_1b.id, aws_default_subnet.default_1c.id ]
+
+  tags = {
+    Name = "TerraformManaged"
+  }
+}
+
+resource "aws_lb_listener" "master_listener" {
+  load_balancer_arn = aws_lb.master_lb.arn
+  port              = "8443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.master_tg.arn
+  }
+}
+
+/*
+resource "aws_lb_listener_rule" "master_rule" {
+  listener_arn = aws_lb_listener.master_listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.master_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+}
+*/
+
+resource "aws_lb_target_group" "master_tg" {
+  name     = "mastertg"
+  port     = 8443
+  protocol = "HTTPS"
+  vpc_id   = aws_default_vpc.default_vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "master_tg_attachment" {
+  target_group_arn = aws_lb_target_group.master_tg.arn
+  target_id        = aws_instance.master_server.id
+  port             = 8443
+}
+
 resource "null_resource" "wait_for_resource" {
   provisioner "local-exec" {
-    command = "sleep 30"
+    command = "sleep 60"
   }
 }
 
