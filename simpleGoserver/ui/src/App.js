7731aab8361f0e5298f3ab2153a9eb95f8637318
manickaraj4 <manick.rajm@gmail.com>
2025-07-07 09:38:38 +0530

    Revert "Adding file storage"
    This reverts commit efbcc9d4bf3cadab74c8e93448f323ebd03721f2.

--- simpleGoserver/ui/src/App.js
+++ simpleGoserver/ui/src/App.js
@@ -66,7 +66,7 @@ function App() {
           </div>
     </NavItem>
       </Nav>
-       <FileUploadComponent></FileUploadComponent>
+      
       </div>
     )
   }
--- simpleGoserver/ui/src/components/fileupload.js
+++ simpleGoserver/ui/src/components/fileupload.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
-
 
 const uploaddata = async (formData) => {
 
@@ -46,7 +44,7 @@ function FileUploadComponent() {
   return (
     <div>
       <Form.Group controlId="formFileLg" className="mb-3">
-        <Form.Label>Select File to Upload</Form.Label>
+        <Form.Label>Select File to Upload {filetype}</Form.Label>
         <Form.Control type="file" size="sm" onChange={handleFileChange}/>
       </Form.Group>
       <button onClick={handleUpload}>Upload File</button>
